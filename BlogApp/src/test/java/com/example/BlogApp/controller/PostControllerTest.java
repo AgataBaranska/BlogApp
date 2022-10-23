436b218d78b3810ddbccfe7e4eda0af3bef74f57
AgataBaranska <baranskagata@gmail.com>
2022-10-23 14:26:49 +0200

    controller tests

--- BlogApp/src/test/java/com/example/BlogApp/controller/PostControllerTest.java
+++ BlogApp/src/test/java/com/example/BlogApp/controller/PostControllerTest.java
@@ -2,6 +2,7 @@ package com.example.BlogApp.controller;
 
 import com.example.BlogApp.dao.PostRepository;
 import com.example.BlogApp.dto.input.PostDtoIn;
+import com.example.BlogApp.entity.Post;
 import com.example.BlogApp.service.PostService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.*;
@@ -32,6 +33,8 @@ class PostControllerTest {
     public static final MediaType APPLICATION_JSON_UTF8 = MediaType.APPLICATION_JSON;
     private static final String EXAMPLE_TEXT = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.  ulpa qui officia deserunt mollit anim id est laborum";
     private static final String EXAMPLE_TITLE = "Super story";
+    private static final long ID = 1L;
+
     private static MockHttpServletRequest request;
     @Autowired
     ObjectMapper objectMapper;
@@ -39,13 +42,30 @@ class PostControllerTest {
     private MockMvc mockMvc;
 
     @Autowired
-    private  PostService postService;
+    private PostService postService;
 
     @Autowired
     private PostRepository postRepository;
 
+    private static Stream<Arguments> parametersForTestingShouldTestPagination() {
+        return Stream.of(
+                Arguments.of("Should return 2 elements on second page when pagination parameters are size=2 page=1", "2", "1", 2),
+                Arguments.of("Should return 0 elements on third page when pagination parameters are size=2 page=2", "2", "2", 0),
+                Arguments.of("Should return 0 elements on first page when pagination parameters are size=3 page=0", "3", "0", 3),
+                Arguments.of("Should return 1 elements on second page when pagination parameters are size=3 page=1", "3", "1", 1)
+        );
+    }
+
+    public static String asJsonString(final Object obj) {
+        try {
+            return new ObjectMapper().writeValueAsString(obj);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     @BeforeEach
-   void setup() {
+    void setup() {
         PostDtoIn post = PostDtoIn.builder().title("Title").text("Tekst").build();
         PostDtoIn post1 = PostDtoIn.builder().title("Title1").text("Tekst1").build();
         PostDtoIn post2 = PostDtoIn.builder().title("Title2").text("Tekst2").build();
@@ -58,11 +78,11 @@ class PostControllerTest {
     }
 
     @AfterEach
-    void teardown(){
+    void teardown() {
         postRepository.deleteAll();
     }
 
-    @ParameterizedTest(name="{0}")
+    @ParameterizedTest(name = "{0}")
     @MethodSource("parametersForTestingShouldTestPagination")
     void shouldTestPagination(String name, String size, String page, Integer expectedElementsCount) throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.get("/api/posts")
@@ -73,19 +93,10 @@ class PostControllerTest {
                 .andExpect(MockMvcResultMatchers.jsonPath("$.content", hasSize(expectedElementsCount)));
     }
 
-    private static Stream<Arguments> parametersForTestingShouldTestPagination(){
-        return  Stream.of(
-                Arguments.of("Should return 2 elements on second page when pagination parameters are size=2 page=1", "2", "1",2),
-                Arguments.of("Should return 0 elements on third page when pagination parameters are size=2 page=2", "2", "2",0),
-                Arguments.of("Should return 0 elements on first page when pagination parameters are size=3 page=0", "3", "0",3),
-                Arguments.of("Should return 1 elements on second page when pagination parameters are size=3 page=1", "3", "1",1)
-        );
-    }
-
     @Test
     void shouldCreatePost() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.post("/api/posts")
-                .content(asJsonString(arrangePostDtoIn()))
+                .content(asJsonString(arrangePostDtoIn(null)))
                 .contentType(MediaType.APPLICATION_JSON)
                 .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isCreated())
@@ -93,24 +104,29 @@ class PostControllerTest {
                 .andExpect(jsonPath("$.text").value(EXAMPLE_TEXT));
     }
 
-    public static String asJsonString(final Object obj) {
-        try {
-            return new ObjectMapper().writeValueAsString(obj);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+    @Test
+    void shouldUpdatePost() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.put("/api/posts")
+                .content(asJsonString(arrangePostDtoIn(ID)))
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.title").value(EXAMPLE_TITLE))
+                .andExpect(jsonPath("$.text").value(EXAMPLE_TEXT));
     }
 
     @Test
-    void update() {
+    void shouldDeletePost() throws Exception {
+        //given
+        Post savedPost = postRepository.save(Post.from(arrangePostDtoIn(null)));
+        //when //then
+        mockMvc.perform(MockMvcRequestBuilders.delete("/api/posts/{id}", savedPost.getId()))
+                .andExpect(status().isNoContent());
     }
 
-    @Test
-    void delete() {
-    }
-
-    private PostDtoIn arrangePostDtoIn() {
+    private PostDtoIn arrangePostDtoIn(Long id) {
         return PostDtoIn.builder()
+                .id(id)
                 .text(EXAMPLE_TEXT)
                 .title(EXAMPLE_TITLE)
                 .build();
